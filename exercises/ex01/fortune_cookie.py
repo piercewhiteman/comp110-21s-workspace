eef662ba063376db29c98ac919da87e871874817
Pierce Whiteman <piercew@email.unc.edu>
2021-02-06 15:50:50 -0500

    Finished Part 1

--- exercises/ex01/fortune_cookie.py
+++ exercises/ex01/fortune_cookie.py
@@ -1,6 +1,6 @@
 """Program that outputs one of at least four random, good fortunes."""
 
-__author__ = "ENTER YOUR 9-DIGIT PID HERE"
+__author__ = "730393281"
 
 # The randint function is imported from the random library so that
 # you are able to generate integers at random.
@@ -12,4 +12,28 @@ __author__ = "ENTER YOUR 9-DIGIT PID HERE"
 from random import randint
 
 
+
 # Begin your solution here...
+def my_fortune (x: int) -> str:
+    fortune : str = ""
+    if x == 1:
+        fortune = "You will receive all A's this semester."
+        return fortune
+    else:
+        if x == 2:
+            fortune = "Their will be sunny weather today."
+            return fortune
+        else:
+            if x == 3:
+                fortune = "You will find love on Valentine's Day."
+                return fortune
+            else:
+                fortune = "You will recieve great wealth in the coming days."
+                return fortune
+
+x : int = randint (1, 4)
+print ("Your fortune cookie says...")
+print (my_fortune(x))
+print ("Now, go spread positive vibes")
+
+
---
+++ lessons/input.py
@@ -0,0 +1,5 @@
+"""A program to demonstrate user input and variable"""
+
+name = str (input("Who are you? "))
+print ("Wow, " + name + ", you rock!")
+print (name + " have a great day!")
